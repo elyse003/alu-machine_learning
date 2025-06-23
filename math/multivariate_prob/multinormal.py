341c19a4eb2559a8e6ae2226b249d4106e72a96d
elyse003 <e.uyiringiy@alustudent.com>
2025-06-23 15:10:45 +0200

    multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -43,4 +43,15 @@ class MultiNormal:
         if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
-        if len(x.shape) != 2
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        determinant = np.linalg.det(self.cov)
+        inverse = np.linalg.inv(self.cov)
+        first = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(determinant))
+        second = np.dot((x - self.mean).T, inverse)
+        third = np.dot(second, (x - self.mean) / -2)
+        pdf = first * np.exp(third)
+
+        return pdf[0][0]
+    
